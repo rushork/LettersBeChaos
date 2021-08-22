30bfa5acc9a3b8395ffd1b1b98cb93b948d62da3
tbage <trafford.bage21@gmail.com>
2021-08-22 19:22:51 +0100

    New Folders and Temp Art
    Going to keep making frequent updates so we dont overwrite eachother at any point.
    
    - Added Temporary letter art
    - Working on letter prefabs
    - Added Letter SO but i havent customised their values, only their names.

---
+++ Letters Be Chaos/Assets/Scripts/LetterSO.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newLetter", menuName = "Letter Item")]
+public class LetterSO : ScriptableObject
+{
+    [Header("Unique Information")]
+    public string nameString;
+    [Tooltip("Does this letter need a tracking number?")]public bool isTracked;
+    [Header("Spawning Chances")]
+    [Range(0, 1)] public float chanceToSpawnSealed;
+    [Tooltip("The chance that this letter type will be stamped as intended on spawn")]
+    public float chanceToBeCorrectlyStamped;
+    [Tooltip("The chance of this letter having a seal with this colour")]
+    [Header("Colour Chances")]
+    [Range(0, 1)] public float chanceGreen;
+    [Range(0, 1)] public float chanceRed;
+    [Range(0, 1)] public float chanceBlue;
+}
