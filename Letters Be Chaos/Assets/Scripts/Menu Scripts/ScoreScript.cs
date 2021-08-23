9fb667fa500ca15dbac11dbaf6419fb48d493757
alexmorris7 <alexandermorris353@gmail.com>
2021-08-23 22:21:27 +0800

    Added Score to final screens
    Used player prefs, set to 0 in the same script

--- Letters Be Chaos/Assets/Scripts/Menu Scripts/ScoreScript.cs
+++ Letters Be Chaos/Assets/Scripts/Menu Scripts/ScoreScript.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public TextMeshProUGUI tmp;
+
+    void Start() {
+        tmp.SetText("Score: " + PlayerPrefs.GetInt("Score"));
+        PlayerPrefs.SetInt("Score", 0);
     }
 }
