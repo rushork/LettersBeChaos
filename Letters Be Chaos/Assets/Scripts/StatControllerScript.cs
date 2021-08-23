589b7d75375e1387213aa507e3c16bb922699dea
alexmorris7 <alexandermorris353@gmail.com>
2021-08-24 05:34:01 +0800

    Added Usage Stats

---
+++ Letters Be Chaos/Assets/Scripts/StatControllerScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatControllerScript : MonoBehaviour
+{
+
+    private int numOfBars = 3;
+
+    public int usage;
+    public int barsActive;
+    public GameObject[] bars;
+    public Sprite barSprite;
+    public Sprite emptyBarSprite;
+
+    // Update is called once per frame
+    void Update() {
+
+        if (usage <= 25) {
+            barsActive = 0;
+        } else if (usage > 75) {
+            barsActive = 3;
+        } else if (usage > 50) {
+            barsActive = 2;
+        } else {
+            barsActive = 1;
+        }
+
+        for (int i = 0; i < numOfBars; i++) {
+            if (i < barsActive) {
+                bars[i].GetComponent<SpriteRenderer>().sprite = barSprite;
+            } else {
+                bars[i].GetComponent<SpriteRenderer>().sprite = emptyBarSprite;
+            }
+        }  
+
+    }
+}
