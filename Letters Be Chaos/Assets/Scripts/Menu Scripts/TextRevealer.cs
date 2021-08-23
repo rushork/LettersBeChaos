d88ce18668d8c2e771c05abc94c77a3ca816ec67
alexmorris7 <alexandermorris353@gmail.com>
2021-08-23 19:55:56 +0800

    Text Revealer Script

---
+++ Letters Be Chaos/Assets/Scripts/Menu Scripts/TextRevealer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextRevealer : MonoBehaviour
+{
+
+    private TextMeshProUGUI tmp;
+
+    IEnumerator Start() {
+        tmp = GetComponent<TextMeshProUGUI>();
+        tmp.ForceMeshUpdate();
+
+        int totalVisibleChars = tmp.textInfo.characterCount;
+        int counter = 0;
+        int visibleCount = 0;
+
+        while (visibleCount != totalVisibleChars) { // Loop till the entire story is visible
+            visibleCount = counter % (totalVisibleChars+1); 
+            tmp.maxVisibleCharacters = visibleCount;
+
+            if (visibleCount >= totalVisibleChars) {
+                yield return new WaitForSeconds(0.05f);
+            }
+
+            counter += 1;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
+
