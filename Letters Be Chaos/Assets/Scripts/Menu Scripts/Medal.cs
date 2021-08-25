1d088348ceb8551fe6b06163cddcf63eaf489d23
alexmorris7 <alexandermorris353@gmail.com>
2021-08-25 16:54:04 +0800

    Medal Room Completed

---
+++ Letters Be Chaos/Assets/Scripts/Menu Scripts/Medal.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Medal : MonoBehaviour
+{
+
+    [Header("Info")]
+    public string medalName;
+    public int unlocked;
+    [TextArea(5, 10)]
+    public string info;
+
+    [Space(1)]
+    [Header("Images")]
+    public Sprite unlockedSprite;
+    public Sprite lockedSprite;
+
+    [Space(1)]
+    public TextMeshProUGUI medalText;
+
+    void Start() {
+        unlocked = PlayerPrefs.GetInt(medalName);
+
+        if (unlocked == 1) {
+            GetComponent<Image>().sprite = unlockedSprite;
+        } else {
+            GetComponent<Image>().sprite = lockedSprite;
+        }
+    }
+
+    public void onMouseHover() {
+        if (unlocked == 1) {
+            medalText.SetText(info);
+        } else {
+            medalText.SetText("???");
+        }
+    }
+
+}
