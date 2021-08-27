b41330c63e5032076b91d187a709a4bac91e7ff0
tbage <trafford.bage21@gmail.com>
2021-08-27 17:58:54 +0100

    Added New Main Menu

--- Letters Be Chaos/Assets/Scripts/Menu Scripts/MuteAudio.cs
+++ Letters Be Chaos/Assets/Scripts/Menu Scripts/MuteAudio.cs
@@ -7,8 +7,17 @@ public class MuteAudio : MonoBehaviour
 
     public AudioSource sound;
 
-    public void Skipped() {
-        sound.Stop();
-        Destroy(this.gameObject);
+    public void Skipped()
+    {
+        if (sound.volume == 1)
+        {
+            sound.volume = 0;
+
+
+        }
+        else
+        {
+            sound.volume = 1;
+        }
     }
-}
+    }
